103ce2692780d96a63abd3a7e1e6ef631126ff5b
Farshad Rezapour <47241600+farshadrp@users.noreply.github.com>
2024-09-27 21:45:14 +0330

    UPDATE server.js 🔧

--- server.js
+++ server.js
@@ -12,10 +12,7 @@ const router = jsonServer.router(db);
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}));
+server.use(jsonServer.rewriter());
 server.use(router);
 server.listen(3000, () => {
   console.log('JSON Server is running');
