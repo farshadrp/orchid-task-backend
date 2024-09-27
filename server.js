d762dadd3c1595d7852136ae97a6416d011bf21f
Farshad Rezapour <47241600+farshadrp@users.noreply.github.com>
2024-09-27 21:33:07 +0330

    UPDATE server.js 🔧

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
+const fs = require('fs')
+const path = require('path')
 const jsonServer = require('json-server');
+
 const server = jsonServer.create();
-const router = jsonServer.router('db/db.json');
+
+const filePath = path.join('db/db.json');
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+
+const router = jsonServer.router(db);
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}));
 server.use(router);
 server.listen(3000, () => {
   console.log('JSON Server is running');
